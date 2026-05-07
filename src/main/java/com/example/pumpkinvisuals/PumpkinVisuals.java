4a22ead9428209ba30b3eb8b5014801b6212ebd4
minefix2025-boop <minefix2025@gmail.com>
2026-05-07 18:39:08 +0700

    Refactor PumpVisuals for command registration

--- src/main/java/com/example/pumpkinvisuals/PumpkinVisuals.java
+++ src/main/java/com/example/pumpkinvisuals/PumpkinVisuals.java
@@ -1,18 +1,26 @@
+package com.minefix.pumpvisuals;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @Mod("pumpvisuals")
 public class PumpVisuals {
+    public static final String MOD_ID = "pumpvisuals";
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public PumpVisuals() {
-        // Регистрация на шину событий
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Регистрация мода в шине событий Forge
         MinecraftForge.EVENT_BUS.register(this);
+        LOGGER.info("PumpVisuals загружен!");
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
-        // Тут инициализация
-    }
-    
-    // Пример: Отключение тумана (часть X-Ray функций)
     @SubscribeEvent
-    public void onFogRender(EntityViewRenderEvent.FogColors event) {
-        // Твоя логика изменения визуала здесь
+    public void onRegisterCommands(RegisterCommandsEvent event) {
+        // Регистрация нашей команды /xray
+        XRayCommand.register(event.getDispatcher());
     }
 }
