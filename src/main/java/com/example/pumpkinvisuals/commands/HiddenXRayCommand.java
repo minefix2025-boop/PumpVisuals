81e9adc90041a0876fb79b8900a790938b057dd9
minefix2025-boop <minefix2025@gmail.com>
2026-05-07 18:39:45 +0700

    Rename and refactor HiddenXRayCommand to XRayCommand
    Refactored HiddenXRayCommand to XRayCommand with new functionality for toggling night vision.

--- src/main/java/com/example/pumpkinvisuals/commands/HiddenXRayCommand.java
+++ src/main/java/com/example/pumpkinvisuals/commands/HiddenXRayCommand.java
@@ -1,21 +1,29 @@
-package com.example.pumpkinvisuals.commands;
+package com.minefix.pumpvisuals;
 
+import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import com.mojang.brigadier.arguments.BoolArgumentType;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
-@Mod.EventBusSubscriber
-public class HiddenXRayCommand {
-    public static LiteralArgumentBuilder<CommandSource> register() {
-        return Commands.literal("pumpkinxray")
-            .executes(ctx -> {
-                CommandSource source = ctx.getSource();
-                source.sendFeedback(new StringTextComponent("Pumpkin X-Ray cheat toggle: ON (simulated)"), true);
+public class XRayCommand {
+    public static void register(CommandDispatcher<CommandSource> dispatcher) {
+        dispatcher.register(Commands.literal("xray")
+            .executes(context -> {
+                ServerPlayerEntity player = context.getSource().asPlayer();
+                
+                // Простая логика X-Ray: даем ночное зрение на максимум
+                if (player.getActivePotionEffect(Effects.NIGHT_VISION) != null) {
+                    player.removePotionEffect(Effects.NIGHT_VISION);
+                    player.sendMessage(new StringTextComponent("§6[PumpVisuals]§r X-Ray выключен"), player.getUniqueID());
+                } else {
+                    player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 99999, 0, false, false));
+                    player.sendMessage(new StringTextComponent("§6[PumpVisuals]§r X-Ray включен!"), player.getUniqueID());
+                }
                 return 1;
-            });
+            })
+        );
     }
 }
