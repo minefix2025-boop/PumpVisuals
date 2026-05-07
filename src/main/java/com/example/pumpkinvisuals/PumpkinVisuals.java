f854f8a760c7e12af64c54778fa0befe15f7c216
minefix2025-boop <minefix2025@gmail.com>
2026-05-07 17:41:00 +0700

    Create PumpkinVisuals class for mod initialization

---
+++ src/main/java/com/example/pumpkinvisuals/PumpkinVisuals.java
@@ -0,0 +1,12 @@
+package com.example.pumpkinvisuals;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("pumpkinvisuals")
+public class PumpkinVisuals {
+    public static final String MODID = "pumpkinvisuals";
+
+    public PumpkinVisuals() {
+        // Initialization placeholder
+    }
+}
