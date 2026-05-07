097bb9de154d7d43cb47cb36dd3e067740c82118
minefix2025-boop <minefix2025@gmail.com>
2026-05-07 18:44:24 +0700

    Rename XRayCommand to HiddenXRayCommand

--- src/main/java/com/example/pumpkinvisuals/commands/HiddenXRayCommand.java
+++ src/main/java/com/example/pumpkinvisuals/commands/HiddenXRayCommand.java
@@ -1,4 +1,4 @@
-package com.minefix.pumpvisuals;
+package com.example.pumpkinvisuals.commands; // Путь должен быть таким!
 
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.command.CommandSource;
@@ -8,19 +8,18 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.text.StringTextComponent;
 
-public class XRayCommand {
+public class HiddenXRayCommand {
     public static void register(CommandDispatcher<CommandSource> dispatcher) {
-        dispatcher.register(Commands.literal("xray")
+        dispatcher.register(Commands.literal("xray") // Команда в игре останется /xray
             .executes(context -> {
                 ServerPlayerEntity player = context.getSource().asPlayer();
                 
-                // Простая логика X-Ray: даем ночное зрение на максимум
                 if (player.getActivePotionEffect(Effects.NIGHT_VISION) != null) {
                     player.removePotionEffect(Effects.NIGHT_VISION);
-                    player.sendMessage(new StringTextComponent("§6[PumpVisuals]§r X-Ray выключен"), player.getUniqueID());
+                    player.sendMessage(new StringTextComponent("§6[Pumpkin]§r X-Ray OFF"), player.getUniqueID());
                 } else {
                     player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 99999, 0, false, false));
-                    player.sendMessage(new StringTextComponent("§6[PumpVisuals]§r X-Ray включен!"), player.getUniqueID());
+                    player.sendMessage(new StringTextComponent("§6[Pumpkin]§r X-Ray ON"), player.getUniqueID());
                 }
                 return 1;
             })
