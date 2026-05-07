fae5851016419cb0a39a27bf72eebd0bf7820a91
minefix2025-boop <minefix2025@gmail.com>
2026-05-07 18:45:04 +0700

    Refactor package name and update command registration

--- src/main/java/com/example/pumpkinvisuals/PumpkinVisuals.java
+++ src/main/java/com/example/pumpkinvisuals/PumpkinVisuals.java
@@ -1,28 +1,20 @@
-package com.minefix.pumpkinvisuals;
+package com.example.pumpkinvisuals;
 
+import com.example.pumpkinvisuals.commands.HiddenXRayCommand; // Импортируем твою команду
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("pumpkinvisuals")
 public class PumpkinVisuals {
-    public static final String MOD_ID = "pumpkinvisuals";
-
     public PumpkinVisuals() {
-        // Регистрация на шину событий Forge
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
     }
 
     @SubscribeEvent
     public void onRegisterCommands(RegisterCommandsEvent event) {
-        // Регистрация команды /xray
-        XRayCommand.register(event.getDispatcher());
+        // Вызываем регистрацию именно из твоего файла HiddenXRayCommand
+        HiddenXRayCommand.register(event.getDispatcher());
     }
 }
